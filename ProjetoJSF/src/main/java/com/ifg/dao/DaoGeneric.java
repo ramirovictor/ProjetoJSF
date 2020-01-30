f92d59333714b476c8291fbccf8cdf16bde33002
Ramiro Victor <ramirovictor3@gmail.com>
2020-01-30 14:10:48 -0200

    Completando o CRUD

--- ProjetoJSF/src/main/java/com/ifg/dao/DaoGeneric.java
+++ ProjetoJSF/src/main/java/com/ifg/dao/DaoGeneric.java
@@ -1,5 +1,7 @@
 package com.ifg.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
@@ -32,15 +34,46 @@ public class DaoGeneric<E> {
 		entityManager.close();
 		return retorno;
 	}
+
 	public void delete(E entity) {
 		EntityManager entityManager = JPAUtil.getEntityManager();
 		EntityTransaction entityTransaction = entityManager.getTransaction();
 		entityTransaction.begin();
-		
+
 		entityManager.remove(entity);
-		 
+
+		entityTransaction.commit();
+		entityManager.close();
 		entityTransaction.commit();
 		entityManager.close();
 	}
 
+	public void deletePorId(E entity) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+
+		Object id = JPAUtil.getPrimaryKey(entity);
+		entityManager.createQuery("delete from " + entity.getClass().getCanonicalName() + " where id = " + id)
+				.executeUpdate();
+
+		entityTransaction.commit();
+		entityManager.close();
+	}
+
+	
+	public List<E> getListEntity (Class<E> entidade){
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		List<E> retorno = entityManager.createQuery(
+				"from "+ entidade.getName()).getResultList();
+		
+		entityTransaction.commit();
+		entityManager.close();
+		return retorno;
+	
+		
+	}
 }
--- ProjetoJSF/src/main/java/com/ifg/jsf/PessoaBean.java
+++ ProjetoJSF/src/main/java/com/ifg/jsf/PessoaBean.java
@@ -1,5 +1,9 @@
 package com.ifg.jsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -12,15 +16,29 @@ public class PessoaBean {
 
 	private Pessoa pessoa = new Pessoa();
 	private DaoGeneric<Pessoa> daoGeneric = new DaoGeneric<Pessoa>();
+	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
 
 	public String salvar() {
 		pessoa = daoGeneric.merge(pessoa);
+		carregarPessoas();
 		return "";
 	}
 
 	public String novo() {
 		pessoa = new Pessoa();
 		return "";
+	}
+
+	public String remove() {
+		daoGeneric.deletePorId(pessoa);
+		pessoa = new Pessoa();
+		carregarPessoas();
+		return "";
+	}
+	
+	@PostConstruct 
+	public void carregarPessoas() {
+		pessoas = daoGeneric.getListEntity(Pessoa.class);
 	}
 
 	public Pessoa getPessoa() {
@@ -37,6 +55,10 @@ public class PessoaBean {
 
 	public void setDaoGeneric(DaoGeneric<Pessoa> daoGeneric) {
 		this.daoGeneric = daoGeneric;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
 	}
 
 }
